87fb6ebc77a084bb5606addf6d09fc62cff5ad0f
Safira <safiracamilo@gmail.com>
2022-06-26 18:29:37 +0100

    Projeto Petz Page Objects - SauceLabs

--- pages/consulta_page.py
+++ pages/consulta_page.py
@@ -3,7 +3,6 @@ from pages.base_page import BasePage
 
 
 class ConsultaPage(BasePage):
-
     _search_input = {'by': By.ID, 'value': 'search'}
     _search_button = {'by': By.CLASS_NAME, 'value': 'button-search'}
     _search_text = {'by': By.CLASS_NAME, 'value': 'product-title'}
@@ -18,9 +17,8 @@ class ConsultaPage(BasePage):
         self._escrever(self._search_input, search)
         self._clicar(self._search_button)
 
-
     def pesquisar(self):
         return self._aparecer(self._search_text, 10)
 
-    #def encontrartext(self):
-        #return self._encontrartext(self._get_text)
+    # def encontrartext(self):
+    # return self._encontrartext(self._get_text)
